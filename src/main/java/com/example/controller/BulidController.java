44f4a5158f0901900f4af8ba0bfad66cd994d801
Your Name <you@example.com>
2024-07-25 11:05:57 +0800

    eazy excel

--- src/main/java/com/example/controller/BulidController.java
+++ src/main/java/com/example/controller/BulidController.java
@@ -1,16 +1,23 @@
 package com.example.controller;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.entities.Build;
 import com.example.entities.Employments;
+import com.example.mapper.BuildMapper;
 import com.example.models.BuildModel;
 import com.example.models.EmploymentModel;
 import com.example.service.BuildSevice;
 import com.example.utils.RestResponse;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,18 +27,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BulidController {
     public final BuildSevice buildSevice;
+    public final BuildMapper buildMapper;
+
     @RequestMapping("/getLists")
-    public RestResponse<Page<Build>> getAllBuild(BuildModel param){
+    public RestResponse<Page<Build>> getAllBuild(BuildModel param) {
         Page<Build> bulidPage = buildSevice.getLists(param);
         return RestResponse.ok(bulidPage);
     }
+
     @RequestMapping("/getEmpById")
-    public RestResponse<Build> getEmpById(String bid){
+    public RestResponse<Build> getEmpById(String bid) {
         List<Build> alist = new ArrayList<>();
-        if (bid == null || bid.equals("")){
+        if (bid == null || bid.equals("")) {
             return RestResponse.fail("ID不能为空值哦！！！");
-        }
-        else {
+        } else {
             Build empOne = buildSevice.getById(bid);
             alist.add(empOne);
             return RestResponse.ok(empOne);
@@ -39,23 +48,57 @@ public class BulidController {
     }
 
     @RequestMapping("/add")
-    public RestResponse addBulid(BuildModel param){
+    public RestResponse addBulid(BuildModel param) {
         Build addResult = buildSevice.addBuild(param);
-        if (addResult != null){
+        if (addResult != null) {
             return RestResponse.ok(param);
         }
         return RestResponse.fail();
     }
+
     @RequestMapping("update")
-    public RestResponse updateBuild(BuildModel param){
+    public RestResponse updateBuild(BuildModel param) {
         String upStr = buildSevice.updateBuild(param);
-        if (upStr != null){
+        if (upStr != null) {
             return RestResponse.fail();
         }
         return RestResponse.ok(param);
     }
+
     @RequestMapping("delete")
-    public RestResponse deleteBuild(String bid){
+    public RestResponse deleteBuild(String bid) {
         return buildSevice.delBuildById(Integer.parseInt(bid));
+    }
+
+    /**
+     * excel文件导出接口
+     *
+     * @param response
+     * @throws Exception
+     */
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws Exception {
+        //获取要导出的数据
+        ArrayList<Build> builds = buildMapper.selectAllBuild();
+        //用excel写入器,写到浏览器
+        try (ExcelWriter writer = ExcelUtil.getWriter(true)) {
+            //一次写到excel
+            writer.write(builds, true);
+            //设置浏览器响应格式
+            response.setContentType("application/vnd.openxmlformats-officedocument." +
+                    "spreadsheetml.sheet;charset=utf-8");
+            String fileName = URLEncoder.encode("建筑信息", "UTF-8");
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName +
+                    ".xlsx");
+            //返回到浏览器
+            try (ServletOutputStream outputStream = response.getOutputStream()) {
+                //刷新到数据流中
+                writer.flush(outputStream, true);
+
+                outputStream.close();
+
+                writer.close();
+            }
+        }
     }
 }
--- src/main/java/com/example/mapper/BuildMapper.java
+++ src/main/java/com/example/mapper/BuildMapper.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.entities.Build;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.ArrayList;
+
 @Mapper
 public interface BuildMapper extends BaseMapper<Build> {
+    ArrayList<Build> selectAllBuild();
 }
